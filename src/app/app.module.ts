33a1746a52d8252a4186fa360413db53f5365809
Maratib Ali Khan <maratib@gmail.com>
2023-09-23 04:35:10 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { PagesComponent } from './pages/pages.component';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HomeComponent,
+    // AboutComponent,
+    PagesComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
